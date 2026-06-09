4df0c5722ff9e90fabb4c54e86a452fee910ce6d
Andy Rollins <andy.rollins@example.com>
2026-06-08 21:09:16 -0500

    installed vuetify packages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/js'
+
 import App from './App.vue'
 import router from './router'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'light',
+  },
+})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
