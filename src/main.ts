db9f39764587d30e9e90674df67ce6749f3d1d38
Andy Rollins <andy.rollins@example.com>
2026-06-08 21:20:29 -0500

    fix: use MDI font for proper icon rendering

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
-import '@mdi/js'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 import router from './router'
@@ -16,10 +15,6 @@ const vuetify = createVuetify({
   directives,
   icons: {
     defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-    },
   },
   theme: {
     defaultTheme: 'light',
